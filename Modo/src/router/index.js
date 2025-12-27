6c7a4388eea5810e26ee7050f61fc27c66b6b568
Miguyy <miguelangelo123machado@gmail.com>
2025-12-27 21:24:53 +0000

    feat(Protected Pages): Added the following features: requirements for pages, when the user logs out from the app, he cannot access it, unless he logs in again. Created some Views.

--- Modo/src/router/index.js
+++ Modo/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores/userStore.js'
+
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
 import LandingPageView from '../views/LandingPageView.vue'
 import HabitManagerView from '../views/HabitManagerView.vue'
+import HabitsCreationView from '@/views/HabitsCreationView.vue'
+import SettingsView from '@/views/SettingsView.vue'
+import StatisticsView from '@/views/StatisticsView.vue'
+import AdminPanelView from '@/views/AdminPanelView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,23 +17,65 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: LandingPageView,
+      meta: { requiresLogin: false },
     },
     {
       path: '/signin',
       name: 'signin',
       component: RegisterView,
+      meta: { requiresLogin: false },
     },
     {
       path: '/login',
       name: 'login',
       component: LoginView,
+      meta: { requiresLogin: false },
     },
     {
       path: '/habitsmanager',
       name: 'habitsmanager',
       component: HabitManagerView,
+      meta: { requiresLogin: true },
+    },
+    {
+      path: '/habitscreation',
+      name: 'habitscreation',
+      component: HabitsCreationView,
+      meta: { requiresLogin: true },
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView,
+      meta: { requiresLogin: true },
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      component: StatisticsView,
+      meta: { requiresLogin: true },
+    },
+    {
+      path: '/adminpanel',
+      name: 'adminpanel',
+      component: AdminPanelView,
+      meta: { requiresLogin: true, requiresAdmin: true },
     },
   ],
 })
 
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  if (to.meta.requiresLogin && !userStore.loggedUserId) {
+    next('/login')
+    return
+  }
+
+  if (to.meta.requiresAdmin && !userStore.isAdmin) {
+    next({ name: '/home' })
+    return
+  }
+
+  next()
+})
 export default router
